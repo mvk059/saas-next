3b365022a6df2c02b4f68112503351f110ebfd02
Manpreet Kunnath <manpreetkunnath@gmail.com>
2025-06-19 08:23:46 +0530

    Add CompanionsList & CTA

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import CompanionsCard from "@/components/CompanionsCard";
 import CompanionsList from "@/components/CompanionsList";
 import Cta from "@/components/CTA";
+import {recentSessions} from "@/constants";
 
 const Page = () => {
 	return (
@@ -36,7 +37,11 @@ const Page = () => {
 			</section>
 
 			<section className="home-section">
-				<CompanionsList />
+				<CompanionsList
+					title="Recently completed sessions"
+					companions={recentSessions}
+					classNames="w-2/3 max-lg:w-full"
+				/>
 				<Cta />
 			</section>
 

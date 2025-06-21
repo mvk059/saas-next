3221925f2c9914bb3b9fe4e33dd09478d4616baa
Manpreet Kunnath <manpreetkunnath@gmail.com>
2025-06-21 12:18:15 +0530

    Add Supabase

--- components/CompanionForm.tsx
+++ components/CompanionForm.tsx
@@ -10,6 +10,8 @@ import {Input} from "@/components/ui/input"
 import {Select, SelectContent, SelectItem, SelectTrigger, SelectValue} from "@/components/ui/select";
 import {subjects} from "@/constants";
 import {Textarea} from "@/components/ui/textarea";
+import {createCompanion} from "@/lib/actions/companion.actions";
+import {redirect} from "next/navigation";
 
 const formSchema = z.object({
 	name: z.string().min(1, {message: 'Companion is required.'}),
@@ -35,8 +37,14 @@ const CompanionForm = () => {
 	})
 
 	// 2. Define a submit handler.
-	function onSubmit(values: z.infer<typeof formSchema>) {
-		console.log(values)
+	const onSubmit = async (values: z.infer<typeof formSchema>) => {
+		const companion = await createCompanion(values);
+		if (companion) {
+			redirect(`/companions/${companion.id}`);
+		} else {
+			console.log("Failed to create Companion.");
+			redirect('/');
+		}
 	}
 
 	return (
